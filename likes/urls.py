8dee43dfa381f7db8d475929004418f60630c163
mohamedhdo <mohamed.haddou.4002@gmail.com>
2025-02-03 16:34:16 +0000

    adding like and unlike feature

--- likes/urls.py
+++ likes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from . import views
 
 router = DefaultRouter()
 
 urlpatterns = [
-
+ path('like/<int:id>/',views.like_unlike_blog,name="like")
 ]
